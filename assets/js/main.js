fcdd84ae202467e372216e297c3b8554c21fbfa6
artherzno-pc <artherzno@hotmail.com>
2019-04-19 04:43:00 +0700

    [dt,tl] fix orientation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,11 +12,14 @@ $(document).ready(function(){
   }
 
   // Detect Orientation change /////////////////////////////////////////////////
-  // window.addEventListener("orientationchange", function() {
-  //   if(window.orientation >= 0) {
-  //     $('.dontOrientation').toggleClass('show');
-  //   }
-  // }, false);
+  window.addEventListener("orientationchange", function() {
+    console.log(window.orientation);
+    if(window.orientation != 0) {
+      $('.dontOrientation').addClass('show');
+    } else {
+      $('.dontOrientation').removeClass('show');
+    }
+  }, false);
 
   // Animate Brain /////////////////////////////////////////////////////////////
   var brain = $(".brain"),
