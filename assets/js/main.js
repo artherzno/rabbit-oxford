b7c7860b8014ca08ed1bbea1c68daedfccf140c1
artherzno-mac <artherzno@gmail.com>
2019-07-05 13:47:33 +0700

    [tl] fix bug rotate view.

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,19 +47,17 @@ $(document).ready(function(){
 
   function detectMobileTablet() {
     var winWidthCurr = screen.availWidth;
-    alert(winWidthCurr);
     if(winWidthCurr < 600) {
-      // if( navigator.userAgent.match(/Android/i)
-      // || navigator.userAgent.match(/webOS/i)
-      // || navigator.userAgent.match(/iPhone/i)
-      // || navigator.userAgent.match(/iPad/i)
-      // || navigator.userAgent.match(/iPod/i)
-      // || navigator.userAgent.match(/BlackBerry/i)
-      // || navigator.userAgent.match(/Windows Phone/i)
-      // ){
-      
+      if( navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i)
+      ){
         orientationScreen();
-      // }
+      }
     }
   }
   detectMobileTablet();
